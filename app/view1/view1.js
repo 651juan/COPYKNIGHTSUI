53b35cd55f76548a46a61b21ce21f66dc5ea06da
Juan Buhagiar <juan.buhagiar@iaeste.org.mt>
2016-10-08 20:02:02 +0200

    Fixed Ui

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute','datatables'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -9,10 +9,23 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$http','$scope', function($http, $scope) {
-  $http.get("http://localhost:8080/article?category=Category:Studies&limit=10&getContent=true&cmContinue")
-      .then(function(response){
-          console.log(response['data']['articles']);
-        $scope.articles = response['data']['articles'];
-      });
+.controller('View1Ctrl', ['$http', '$q', 'DTOptionsBuilder', 'DTColumnBuilder', function($http, $q, DTOptionsBuilder, DTColumnBuilder) {
+    var vm = this;
+
+    vm.dtOptions = DTOptionsBuilder.fromFnPromise(getData());
+
+    function getData() {
+        var defer = $q.defer();
+        $http.get('http://localhost:8080/article?category=Category:Studies&limit=10&getContent=true&cmContinue').then(function (result) {
+            defer.resolve(result['data']['articles']);
+        });
+        return defer.promise;
+    }
+
+    vm.dtColumns = [
+        DTColumnBuilder.newColumn('pageid').withTitle('ID'),
+        DTColumnBuilder.newColumn('name').withTitle('Name'),
+        DTColumnBuilder.newColumn('year').withTitle('Year')
+    ];
+
 }]);
