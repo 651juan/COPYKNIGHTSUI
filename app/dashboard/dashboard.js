5c02784d2d2953dc3a12f53d6ee79a25adb55a49
Danielle Duijst <dmsduijst@gmail.com>
2016-11-08 13:46:05 +0100

    I changed the dashboard html and js files. Made 3 columns in the html file Imported title, citations and authors in js file In index.html bootstrap is imported now.

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,52 +1,20 @@
 angular.module('myApp.dashboard', ['ngRoute'])
 
 
-.controller('DashboardCtrl', ['articleService', '$http',function(articleService,$http) {
+    .controller('DashboardCtrl', ['articleService', '$http','$scope',function(articleService,$http, $scope) {
 
-    function init() {
-        $http.get('http://localhost:8080/year').then(function (result) {
-            var data = {
-                labels: [],
-                datasets: [{
-                    label: "My Dataset",
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1,
-                    data: []
-                }]
-            };
-            var keys = Object.keys(result['data']);
-
-            for (var i = 0; i < keys.length; i++) {
-                data.labels.push(keys[i]);
-                data.datasets[0].data.push(result['data'][keys[i]]);
-
-            }
-
-            console.log(data);
-            var ctx = document.getElementById("myChart");
-            var myBarChart = new Chart(ctx, {
-                type: 'bar',
-                data: data,
-                options: {}
+        function init() {
+            $.getJSON("articles.json", function(json) {
+                console.log(json); // this will show the info it in firebug console
+                $scope.citation = json.articles[0].citation;
+                $scope.title = json.articles[0].title;
+                $scope.authors = json.articles[0].authors;
+                $scope.$apply();
             });
-        });
-    }
 
-    init();
+        }
+        init();
 
-}]);
+    }])
+
+
