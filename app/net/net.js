ae57ad7cd078ee4eb989f6bcbeb03401248a4fd5
Adrian Apap <apapadrian@gmail.com>
2016-11-09 20:10:47 +0100

    Fixed NET

--- app/net/net.js
+++ app/net/net.js
@@ -1,108 +1,106 @@
-angular.module('myApp.net', ['ngRoute'])
+angular.module('myApp.net', ['ngRoute']).controller('netCtrl', ['articleService', '$http','$scope',function(articleService,$http, $scope) {
 
+    var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
 
-    .controller('netCtrl', ['articleService', '$http','$scope',function(articleService,$http, $scope) {
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+    var simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function(d) { return d.id; }))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
 
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+    d3.json("http://localhost:8080/article", function(error, result) {
+        if (error) throw error;
+        var articles  = result['articles'];
+        var graph = {};
+        var tmpNodes = Array();
+        var tmpLinks = Array();
 
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+        var allArticlesReferences = [];
 
-d3.json("http://localhost:8080/article", function(error, result) {
-    if (error) throw error;
-    var articles  = result['articles'];
-    var graph = {};
-    var nodes = [];
-    var links = [];
-    var article;
-    var reference;
-    var n = {};
-    for (article in articles) {
-        for (reference in articles[article]['refences']) {
-            var l = {};
-            n[articles[article]['refences'][reference]['title'].trim()] = 1;
-            l['source'] = articles[article]['name'].trim();
-            l['target'] = articles[article]['refences'][reference]['title'].trim();
-            l['value'] = 1;
-            links.push(l);
+        for (var article in articles) {
+            allArticlesReferences[articles[article]['name'].trim()] = 1;
+            for (var reference in articles[article]['references']) {
+                var tmpLink = {};
+                allArticlesReferences[articles[article]['references'][reference].trim()] = 1;
+
+                tmpLink['source'] = articles[article]['name'].trim();
+                tmpLink['target'] = articles[article]['references'][reference].trim();
+                tmpLink['value'] = 1;
+
+                tmpLinks[tmpLinks.length] = tmpLink;
+            }
         }
+
+        for (var node in allArticlesReferences) {
+            var currNode = {};
+            currNode['id'] = node;
+            currNode['group'] = allArticlesReferences[node];
+            tmpNodes.push(currNode);
+        }
+
+        graph.nodes = tmpNodes;
+        graph.links = tmpLinks;
+
+        var link = svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(graph.links)
+            .enter().append("line")
+            .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+        var node = svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("circle")
+            .data(graph.nodes)
+            .enter().append("circle")
+            .attr("r", 5)
+            .attr("fill", function(d) { return color(d.group); })
+            .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
+
+        node.append("title")
+            .text(function(d) { return d.id; });
+
+        simulation
+            .nodes(graph.nodes)
+            .on("tick", ticked);
+
+        simulation.force("link")
+            .links(graph.links);
+
+        function ticked() {
+            link
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            node
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+        }
+    });
+
+    function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
     }
-    var no;
-    for (no in n) {
-        currNode = {};
-        currNode['id'] = no;
-        currNode['group'] = n[no];
-        nodes.push(currNode);
+
+    function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
     }
-    console.log(nodes);
-    graph['nodes'] = nodes;
-    graph['links'] = links;
 
-    console.log(graph);
-
-    var link = svg.append("g")
-        .attr("class", "links")
-        .selectAll("line")
-        .data(graph.links)
-        .enter().append("line")
-        .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
-
-    var node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(graph.nodes)
-        .enter().append("circle")
-        .attr("r", 5)
-        .attr("fill", function(d) { return color(d.group); })
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
-
-    node.append("title")
-        .text(function(d) { return d.id; });
-
-    simulation
-        .nodes(graph.nodes)
-        .on("tick", ticked);
-
-    simulation.force("link")
-        .links(graph.links);
-
-    function ticked() {
-        link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-
-        node
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+    function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
     }
-});
 
-        function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-        }
-
-        function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        }
-
-        function dragended(d) {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            d.fx = null;
-            d.fy = null;
-        }
-
-    }]);
+}]);
