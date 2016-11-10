0a13b192367b448f853cebaada4a82f22e7b9f94
Juan Buhagiar <juanbuhagiar@gmail.com>
2016-11-10 16:50:41 +0000

    Addition of similar articles

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -10,13 +10,15 @@ angular.module('myApp.view2', ['ngRoute'])
                 var ans = result['data']['articles'].filter(function (el) {
                     return el.pageid == $routeParams.id;
                 });
-                console.log(ans);
                 $scope.citation = ans[0].citation;
                 $scope.title = ans[0].title;
                 $scope.authors = ans[0].authors;
 
             });
 
+            $http.get('http://localhost:8080/similar/' + $routeParams.id).then(function (result) {
+                $scope.similar = result['data']['articles'];
+            });
         }
         init();
 }]);
